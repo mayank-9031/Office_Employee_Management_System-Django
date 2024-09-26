059d3777029af9ba7e002a82392d136a6ee1c746
Mayank <mayankgoyal.btprod20@pec.edu.in>
2024-09-26 23:44:38 +0530

    home page and view functions created

--- office_emp_proj/emp_app/urls.py
+++ office_emp_proj/emp_app/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('all_emp', views.index, name='all_emp'),
+    path('add_emp', views.index, name='add_emp'),
+    path('remove_emp', views.index, name='remove_emp'),
+    path('filter_emp', views.index, name='filter_emp'),
 ]
