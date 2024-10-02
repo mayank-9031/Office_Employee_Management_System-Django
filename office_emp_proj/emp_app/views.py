e16cdcce18865849799473a6b024bf2c577726b9
Mayank <mayankgoyal.btprod20@pec.edu.in>
2024-10-02 23:26:10 +0530

    add an employee and view all employee comonent added

--- office_emp_proj/emp_app/views.py
+++ office_emp_proj/emp_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Employee, Role, Department
+from datetime import datetime
 
 # Create your views here.
 
@@ -6,9 +8,27 @@ def index(request):
     return render(request, 'index.html')
 
 def all_emp(request):
-    return render(request, 'view_all_emp.html')
+    emps = Employee.objects.all()
+    context = {
+        "emps" : emps
+    }
+    return render(request, 'view_all_emp.html', context)
 
 def add_emp(request):
+    if request.method == 'POST':
+        # print("POST")
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        salary = int(request.POST['salary'])
+        bonus = int(request.POST['bonus'])
+        phone = int(request.POST['phone'])
+        dept = int(request.POST['dept'])
+        role = int(request.POST['role'])
+        new_emp = Employee(first_name= first_name, last_name=last_name, salary=salary, bonus=bonus, phone=phone, dept_id = dept, role_id = role, hire_date = datetime.now())
+        new_emp.save()
+        return HttpResponse("Employee Added Successfully")
+    else:
+        print("GET")    
     return render(request, 'add_emp.html')
 
 def remove_emp(request):
