1ffd71a131ef310439616631b75bb7de8c71c521
Mayank <mayankgoyal.btprod20@pec.edu.in>
2024-10-02 23:47:37 +0530

    remvove employee functionality added

--- office_emp_proj/emp_app/views.py
+++ office_emp_proj/emp_app/views.py
@@ -27,12 +27,27 @@ def add_emp(request):
         new_emp = Employee(first_name= first_name, last_name=last_name, salary=salary, bonus=bonus, phone=phone, dept_id = dept, role_id = role, hire_date = datetime.now())
         new_emp.save()
         return HttpResponse("Employee Added Successfully")
+    elif request.method == 'GET':
+        return render(request, 'add_emp.html') 
     else:
-        print("GET")    
-    return render(request, 'add_emp.html')
+        return HttpResponse("An Exception Occured")
+    
 
-def remove_emp(request):
-    return render(request, 'remove_emp.html')
+def remove_emp(request, emp_id=0):
+    if emp_id:
+        try:
+            emp_to_be_removed = Employee.objects.get(id=emp_id)
+            emp_to_be_removed.delete()
+            return HttpResponse("Employees removed successfully")
+        except:
+            return HttpResponse("Please Enter A Valide Employee ID")
+        
+    emps = Employee.objects.all()
+    context = {
+        "emps" : emps
+    }
+    return render(request, 'remove_emp.html', context)
+
 
 def filter_emp(request):
     return render(request, 'filter_emp.html')
